94cce66a2ac35b70892a8dd7cbd028e498489aa3
Jaret Balba <Jaretzbalba@gmail.com>
2023-01-19 09:52:31 -0800

    Added UI for goal functions

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,12 @@
 import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import { FaUser } from 'react-icons/fa';
+import { register, reset } from '../features/auth/authSlice';
+import Spinner from '../components/Spinner';
 
-const Register = () => {
+function Register() {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -10,6 +15,23 @@ const Register = () => {
   });
 
   const { name, email, password, password2 } = formData;
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(state => state.auth);
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    if (isSuccess || user) {
+      navigate('/');
+    }
+
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
 
   const onChange = e => {
     setFormData(prevState => ({
@@ -20,7 +42,23 @@ const Register = () => {
 
   const onSubmit = e => {
     e.preventDefault();
+
+    if (password !== password2) {
+      toast.error('Passwords do not match');
+    } else {
+      const userData = {
+        name,
+        email,
+        password,
+      };
+
+      dispatch(register(userData));
+    }
   };
+
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   return (
     <>
@@ -62,7 +100,7 @@ const Register = () => {
               id='password'
               name='password'
               value={password}
-              placeholder='Enter your password'
+              placeholder='Enter password'
               onChange={onChange}
             />
           </div>
@@ -89,6 +127,6 @@ const Register = () => {
       </section>
     </>
   );
-};
+}
 
 export default Register;
