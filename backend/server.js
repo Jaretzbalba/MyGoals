837630a49a69e954986375dab96af8fc1f6a98fe
Jaret Balba <Jaretzbalba@gmail.com>
2023-01-19 14:28:33 -0800

    trying new path for production

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use('/api/users', require('./routes/userRoutes'));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'build')));
 
-  app.get('/', function (req, res) {
+  app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 } else {
